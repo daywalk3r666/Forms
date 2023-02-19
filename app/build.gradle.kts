2da15608af01c22edddf6e478816c26ad0dc03a6
daywalk3r666 <markushelm3@gmail.com>
2023-02-19 11:16:29 +0100

    Migrate to kotlin

---
+++ app/build.gradle.kts
@@ -0,0 +1,68 @@
+plugins {
+    id ("com.android.application")
+    id ("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "de.daywalk3r666.forms"
+    compileSdk = 33
+    buildToolsVersion "33.0.0"
+
+    defaultConfig {
+        applicationId = "de.daywalk3r666.forms"
+        minSdk = 1
+        targetSdk = 33
+        versionCode = 10
+        versionName "1.0"
+        resConfigs = "en"
+            
+signingConfigs {
+        create("gh-actions") {
+            storeFile = file("${System.getenv("RUNNER_TEMP")}/keystore/keystore.jks")
+            storePassword = System.getenv("KEYSTORE_PASSWORD")
+            keyAlias = System.getenv("SIGNING_KEY_ALIAS")
+            keyPassword = System.getenv("SIGNING_KEY_PASSWORD")
+        }
+    } 
+
+    }
+    
+       applicationVariants.all {
+        outputs.all {
+            (this as? ApkVariantOutputImpl)?.outputFileName =
+                "Forms_${versionName}_${buildType.name}.apk"
+        }
+    }
+    
+    buildTypes {
+        debug {
+            minifyEnabled = false
+            shrinkResources = false
+            crunchPngs = false
+            signingConfig = signingConfigs.findByName("gh-actions")
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+
+        release {
+            minifyEnabled = true
+            shrinkResources = true
+            crunchPngs = true
+            signingConfig = signingConfigs.findByName("gh-actions")
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+
+    lintOptions {
+        disable "SetJavaScriptEnabled"
+    }
+
+}
